e331ed065503e59593f2d8647835b6644cbdfc69
Kravets_Dmytro <ditur@ukr.net>
2023-06-26 11:58:18 +0300

    rt

--- src/components/nevLayout/Layout.styled.js
+++ src/components/nevLayout/Layout.styled.js
@@ -2,18 +2,21 @@ import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
 
 export const HeaderContainer = styled.header`
-  display: flex;
-  align-items: center;
-  min-height: 39px;
-  padding: 10px 60px;
-  color: #fff;
-  background-color: #c8f4f7;
+position: sticky;
+  padding:  0 30px;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  margin: 0 auto;
+  background-color: #0a0909;
+  border-bottom: 1px solid #ffffff;
+  background-color: #c6f4f7;
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
 
 export const Link = styled(NavLink)`
-  padding: 0 10px;
+  padding: 0 20px;
   font-size: 18px;
   font-weight: 700;
   letter-spacing: 0.03em;
@@ -41,7 +44,7 @@ export const Link = styled(NavLink)`
       display: block;
       z-index: -1;
       width: 100%;
-      height: 55px;
+      height: 45px;
       background-color: #42362b;
       border-radius: 2px;
 
